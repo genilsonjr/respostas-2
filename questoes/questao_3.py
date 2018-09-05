88a71c7c3404f3f62bf344b3032cc6e408fab222
genilsonjr <42838793+genilsonjr@users.noreply.github.com>
2018-09-05 09:00:31 -0300

    Update questao_3.py

--- questoes/questao_3.py
+++ questoes/questao_3.py
@@ -11,12 +11,12 @@
 # substituindo apenas o comando print(questão...) existente.
 ##
 def main():
-Pi= 3.14 #valor de pi.
-r= int(input('Coloque o valor do raio: '))# pedindo para que o usuario coloque o valor do raio da circunferencia.
-area= Pi * r **2 #calculo da area do circulo.
-diametro= 2 * r #calculo do diametro da circunferencia.
-comprimento= 2 * Pi * r #calculo do comprimento da circunferencia.
-print('Meu amigo a area do seu circulo é', area,'o diametro da sua circunferencia é', diametro,'e o comprimento é', comprimento)#impressao do valor 
+    Pi= 3.14 #valor de pi.
+    r= int(input('Coloque o valor do raio: '))# pedindo para que o usuario coloque o valor do raio da circunferencia.
+    area= Pi * r **2 #calculo da area do circulo.
+    diametro= 2 * r #calculo do diametro da circunferencia.
+    comprimento= 2 * Pi * r #calculo do comprimento da circunferencia.
+    print('Meu amigo a area do seu circulo é', area,'o diametro da sua circunferencia é', diametro,'e o comprimento é', comprimento)#impressao do valor 
 
 
     
