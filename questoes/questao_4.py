e8d90b8a7d579825c7b3d403723fc52e1ae70c48
genilsonjr <42838793+genilsonjr@users.noreply.github.com>
2018-09-05 09:01:16 -0300

    Update questao_4.py

--- questoes/questao_4.py
+++ questoes/questao_4.py
@@ -12,12 +12,12 @@
 ##
 
 def main():
-km= int(input('Quantos qulimetros voce percorreu: '))#pedir que o usuario coloque a quantidade de km pecorridos.
-dias=int(input('Quantos dias voce utilizou o carro: '))#pedir a o usuario a quantidade de dias com o carro.
-resu1= 60 * dias#calculo dos dias 
-resu2= 0.15 * km#calculo dos quilometros
-resutado= resu1 + resu2# a soma dos dois resutados
-print(' O valor a ser pago é R$', resutado)# impressao do valor que o usuario irar pagar.
+    km= int(input('Quantos qulimetros voce percorreu: '))#pedir que o usuario coloque a quantidade de km pecorridos.
+    dias=int(input('Quantos dias voce utilizou o carro: '))#pedir a o usuario a quantidade de dias com o carro.
+    resu1= 60 * dias#calculo dos dias 
+    resu2= 0.15 * km#calculo dos quilometros
+    resutado= resu1 + resu2# a soma dos dois resutados
+    print(' O valor a ser pago é R$', resutado)# impressao do valor que o usuario irar pagar.
 
 
 
